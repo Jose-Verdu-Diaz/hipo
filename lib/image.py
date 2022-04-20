6e7d3c989f4f96d35107a822613b8cf683bbef0b
Jose-Verdu_Diaz <pepv@hotmail.es>
2022-04-20 10:19:26 +0100

    The great code purge

--- lib/image.py
+++ lib/image.py
@@ -29,81 +29,6 @@ from PIL import Image
 from lib.models.Colors import Color
 
 
-def create_gif(images, sample, channels = None):
-    '''Create an animated gif
-
-    Parameters
-    ----------
-    images
-        Numpy array of images
-    sample
-        Name of the sample
-    channels, optional
-        List of channel names, by default None
-    '''
-
-    out = []
-
-    for img in images:
-        converted = Image.fromarray(img).convert('P')
-        #draw = ImageDraw.Draw(converted)
-        #draw.text((50, 50), "Sample Text", 150)
-
-        out.append(converted)      
-
-    out[0].save(f'samples/{sample}/image_animation.gif', save_all=True, append_images=out[1:], optimize=False, duration=1000, loop=0)
-
-
-def show_napari(images, channels):
-
-    color = Color()
-
-    if len(images) > 6:
-        input(f'{color.RED}Only 6 images can be displayed at the same time.\nPress enter to continue...{color.ENDC}')
-        return
-
-    viewer = napari.Viewer()
-
-    colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta']
-
-    for i,img in enumerate(images):
-        cmap = create_cmap(colors[i])
-        viewer.add_image(img, colormap = cmap, name = channels[i])
-
-    print(f'{color.CYAN}Opening napari. Close napari window to continue...{color.ENDC}')
-    napari.run()
-
-
-def create_cmap(c):
-
-    stop = {
-        'red': [1, 0, 0, 1],
-        'yellow': [1, 1, 0, 1],
-        'green': [0, 1, 0, 1],
-        'cyan': [0, 1, 1, 1],
-        'blue': [0, 0, 1, 1],
-        'magenta': [1, 0, 1, 1]
-    }
-
-    colors = np.linspace(
-        start = [0, 0, 0, 1],
-        stop = stop[c],
-        num = 100,
-        endpoint = True
-    )
-
-    colors[0] = np.array([0, 0, 0, 0])
-
-    new_colormap = {
-        'colors': colors,
-        'name': c,
-        'interpolation': 'linear'
-    }
-
-    return new_colormap
-
-
-
 def view_histogram(images, channels, geojson_file):
     img_size = Image.fromarray(images[0]).size
     mask = make_mask(geojson_file, img_size) # make_mask() removed
@@ -155,15 +80,3 @@ def view_histogram(images, channels, geojson_file):
     plt.xticks(rotation = 45)
     plt.subplots_adjust(hspace=.02)
     plt.show()
-
-
-def show_napari_segmentation(img, labels):
-    color = Color()
-
-    viewer = napari.Viewer()
-
-    viewer.add_image(img, name = 'Image')
-    viewer.add_labels(labels, name = 'Image')
-
-    print(f'{color.CYAN}Opening napari. Close napari window to continue...{color.ENDC}')
-    napari.run()
